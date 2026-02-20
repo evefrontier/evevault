3aea1530c0c9843c4ea53357e223cbbabe4a84dc
ccp sondheim <sondheim@ccpgames.com>
2026-02-21 01:00:45 +0800

    bump version

--- apps/extension/wxt.config.ts
+++ apps/extension/wxt.config.ts
@@ -118,7 +118,7 @@ export default defineConfig(() => {
     manifest: {
       key: envVars.EXTENSION_ID,
       name: "EVE Vault",
-      version: "0.0.1",
+      version: "0.0.3",
       description: "EVE Vault for EVE Frontier with ZKLogin",
       permissions: [
         "identity",
