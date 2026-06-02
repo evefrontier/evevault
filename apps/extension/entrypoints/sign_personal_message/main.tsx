64b57f0268367fafe2a0abe6f57705032bc80cb5
ccp sondheim <sondheim@ccpgames.com>
2026-06-02 15:42:37 +0000

    refactor(extension): share sign popup renderer

--- apps/extension/entrypoints/sign_personal_message/main.tsx
+++ apps/extension/entrypoints/sign_personal_message/main.tsx
@@ -1,18 +1,4 @@
-import { Layout, ToastProvider } from '@evevault/shared/components'
-import { queryClient } from '@evevault/shared/queryClient'
-import { QueryClientProvider } from '@tanstack/react-query'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
 import SignPersonalMessage from '@/features/wallet/components/SignPersonalMessage'
+import { renderSignPopup } from '@/renderSignPopup'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <ToastProvider>
-        <Layout variant="extension" showNav={false}>
-          <SignPersonalMessage />
-        </Layout>
-      </ToastProvider>
-    </QueryClientProvider>
-  </React.StrictMode>,
-)
+renderSignPopup(SignPersonalMessage)
